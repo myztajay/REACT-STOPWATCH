22506ed2e0d02a574fdcb1be971db4fa5c69284b
myztajay <myztajay@hotmail.com>
2017-01-31 23:47:47 -0500

    start and stop completed

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -5,14 +5,15 @@ constructor(props){
   super(props);
   this.state = {
     timer: 0
-  }
+  };
 }
 
 
   render(){
+    const {timer} = this.state
     return(
       <div>
-        <h1> hello world</h1>
+        <div className = "timer" > {timer} </div>
         <button onClick={(evt)=> this.StartTimer(evt)}>Start</button>
         <button onClick={(evt)=> this.StopTimer(evt)}>Stop</button>
         <button onClick={(evt)=> this.RestartTimer(evt)}>Restart Timer</button>
@@ -22,13 +23,27 @@ constructor(props){
 
 
   StartTimer(e){
-    console.log("start");
+    var _this= this
+    this.increment = setInterval(function(){
+      _this.setState({
+        timer:  _this.state.timer+ 1
+      })
+    }, 1000)
   }
   StopTimer(e){
     console.log("stop");
+    var _this = this
+    this.increment = clearInterval(this.increment)
   }
   RestartTimer(e){
     console.log("Restart");
+    const {timer} = this.state
+    this.setState({
+        timer: 0
+    })
+  }
+  updateTimer(e){
+    console.log("timerchange");
   }
 
 }
